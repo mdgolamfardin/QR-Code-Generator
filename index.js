2185e039bd6742bff3e65a1726fdba7c1c2600b3
mdgolamfardin <69181182+mdgolamfardin@users.noreply.github.com>
2024-11-16 19:47:15 -0400

    generated qr code for my site

--- index.js
+++ index.js
@@ -3,3 +3,49 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from 'inquirer';
+import fs from "fs";
+import qr from "qr-image"
+
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            name: "url",
+            message: "What is the url of your site?"
+        },
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        console.log(answers.url);
+        fs.writeFile("url2.txt", answers.url, "utf-8", (err) => {
+            if (err) {
+                console.error("Error writing to file:", err);
+            }
+        });
+        const qrThing = qr.image(answers.url, { type: "png" });
+        const output = fs.createWriteStream('qrcode.png');
+        qrThing.pipe(output);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+
+// var url;
+// fs.readFile("url2.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+//     url = data;
+//     console.log("test inside", data);
+
+// });
+
+// console.log("test outside", url);
+
+
