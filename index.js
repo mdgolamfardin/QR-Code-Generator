1e0f82c503be3e26bf02fdb3ba4bbcebf30e0d89
mdgolamfardin <69181182+mdgolamfardin@users.noreply.github.com>
2024-11-16 20:03:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,17 +35,4 @@ inquirer
         }
     });
 
-// var url;
-// fs.readFile("url2.txt", "utf-8", (err, data) => {
-//     if (err) {
-//         console.error('Error reading file:', err);
-//         return;
-//     }
-//     url = data;
-//     console.log("test inside", data);
-
-// });
-
-// console.log("test outside", url);
-
 
